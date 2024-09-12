d508ac144519b280a37fc62dcffc6e9eb3c7ea22
changjun <yuncj5925@gmail.com>
2024-09-12 20:53:48 +0900

    outlet 적용하기 (#1)

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import {
 import Home from './pages/Home'
 import NotFound from './pages/NotFound';
 import Videos from './pages/Videos';
+import Root from './pages/Root';
+
 
 const router = createBrowserRouter([{
   path : '/',
-  element : <Home />,
-  errorElement :<NotFound />
+  element : <Root />,
+  errorElement :<NotFound />,
+  children: [
+    { index: true, element: <Home />},
+    {path : '/videos', element : <Videos />}
+  ]
 },
-{
-  path : '/videos',
-  element : <Videos />
-}
+
 ]);
 
 function App() {
